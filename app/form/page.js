e28a5649065d7c8a0bac057258b622bfc2905859
ArantxaDR <arantxaderuiz@gmail.com>
2023-03-26 17:36:12 +0200

    format the form html, add some styles and start working in the API

---
+++ app/form/page.js
@@ -0,0 +1,39 @@
+'use client'
+import styles from './page.module.css'
+
+export default function Form () {
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const formData = {
+      fullName: event.target.fullName.value,
+      email: event.target.email.value,
+      phone: event.target.phone.value,
+      message: event.target.message.value
+    }
+    fetch('/api/formApi', {
+      method: 'POST',
+      body: JSON.stringify(formData)
+    })
+  }
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+
+      <label className={styles.label} htmlFor='fullName'>Full name
+        <input className={styles.input} required type='text' name='fullName' placeholder='Jane Doe' minLength='3' />
+      </label>
+
+      <label className={styles.label} htmlFor='email'>Email
+        <input className={styles.input} required type='email' name='email' placeholder='example@example.com' minLength='3' />
+      </label>
+
+      <label className={styles.label} htmlFor='phone'>Phone number
+        <input className={styles.input} required type='phone' name='phone' placeholder='555-555-555' minLength='3' />
+      </label>
+
+      <label className={styles.label} htmlFor='message'>Message
+        <textarea className={styles.textarea} required type='text' name='message' placeholder='Send us a message...' minLength='10' />
+      </label>
+      <button type='submit'>Sumbit</button>
+    </form>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,28 +1,15 @@
 // import { Inter } from 'next/font/google'
+import Form from './form/page'
+import Header from './header/page'
 import styles from './page.module.css'
 
 export default function Home () {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <div>
-          <label>Nombre completo: </label>
-          <input type='text' placeholder='Jane Doe' minLength='3' />
-        </div>
-        <div>
-          <label>Email: </label>
-          <input type='email' placeholder='example@example.com' minLength='3' />
-        </div>
-        <div>
-          <label>Teléfono: </label>
-          <input type='phone' placeholder='555-555-555' minLength='3' />
-        </div>
-        <div>
-          <label>Mesaje: </label>
-          <textarea type='text' placeholder='Escriba aquí su mensaje...' minLength='10' />
-        </div>
-        <button>Enviar formilario</button>
-      </div>
-    </main>
+    <>
+      <Header />
+      <main className={styles.main}>
+        <Form />
+      </main>
+    </>
   )
 }
