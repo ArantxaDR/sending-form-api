945bd8c41c97f21685eb07eb0395754af7020457
ArantxaDR <arantxaderuiz@gmail.com>
2023-03-28 18:12:09 +0200

    delete unnecessary comments and code from route.js

--- app/api/form/route.js
+++ app/api/form/route.js
@@ -1,12 +1,8 @@
-import { NextResponse } from 'next/server'
 import { connectMongoDB } from '@/utils/MongoConnect'
 import Form from '@/models/FormModel'
-// import mongoose from 'mongoose'
 
 connectMongoDB()
-export async function GET () {
-  return NextResponse.json({ hello: 'world' })
-}
+
 export async function POST (request) {
   const body = await request.json()
   const newForm = new Form(body)
