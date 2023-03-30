2c1c91af0c04d205f4abc2a4f1b536361295f0c1
ArantxaDR <arantxaderuiz@gmail.com>
2023-03-30 02:31:24 +0200

    change background and title styles and fix responsive

--- app/page.js
+++ app/page.js
@@ -1,15 +1,12 @@
-import Image from 'next/image'
 import Form from './form/page'
 import styles from './page.module.css'
-import send from '../public/send.svg'
 
 export default function Home () {
   return (
     <>
       <main className={styles.main}>
         <section className={styles.title}>
-          <h1>Get in touch!</h1>
-          <Image alt='Send arrow icon' src={send} />
+          <h1 className={styles.text}>Get in touch!</h1>
         </section>
         <Form />
       </main>
