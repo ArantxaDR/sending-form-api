e76fdfacbef0a9a5aa6505a58b31189be1df591c
ArantxaDR <arantxaderuiz@gmail.com>
2023-09-13 11:37:05 +0200

    trying to fix indentation  problem for deploy

--- app/form/page.js
+++ app/form/page.js
@@ -56,11 +56,13 @@ export default function Form () {
       {
         isChecked === false
           ? null
-          : <select name='courtesy' className={styles.courtesy}>
-            <option>Choose your courtesy...</option>
-            <option>Mr.</option>
-            <option>Mrs.</option>
+          : (
+            <select name='courtesy' className={styles.courtesy}>
+              <option>Choose your courtesy...</option>
+              <option>Mr.</option>
+              <option>Mrs.</option>
             </select>
+            )
       }
 
       <ButtonSubmit />
